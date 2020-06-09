d7599418948ab4f97a24b142bc00aa85c982fb2f
Werner Vesterås <werner.vesteraas@marcello.no>
2020-06-09 21:03:33 +0200

    Can now be installed as a CLI tool

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const puppeteer = require('puppeteer');
 const cliProgress = require('cli-progress');
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
